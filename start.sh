4e58b17d19cc4fd25d284fc58623c5a9b856325f
flipjargendy <47167239+flipjargendy@users.noreply.github.com>
2019-04-27 12:38:16 -0500

    Make client.sh executable

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@ renderTime="59" # The max time you are willing to commit to render one frame
 if [ ! -f "client.jar" ]; then
     echo "SheepIt client not found. Downloading the client."
     wget -O client.jar https://www.sheepit-renderfarm.com/media/applet/sheepit-client-5.1292.2977.jar
+    chmod +x client.jar
 fi
 
 # This will run the client with the settings above
