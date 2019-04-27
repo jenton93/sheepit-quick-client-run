3d424c6e296a13f3f1d1e80db951b8cbcf11446d
flipjargendy <47167239+flipjargendy@users.noreply.github.com>
2019-04-27 13:41:35 -0500

    Check if params are set

--- start.sh
+++ start.sh
@@ -6,19 +6,31 @@
 username="x000111" # your SheepIt username
 pass="BMZ4zE7My62vLgjxhtpfgGeSIIiL6afdeoeTh1dR" # your SheepIt password or key
 
-#######################
-# System settings
-#######################
-mem="" # RAM available to SheepIt (
-gpu="--no-gpu" # if you have a GPU leave this empty
-computeMethod="CPU" # leave blank if you are unsure
-renderTime="" # The max time you are willing to commit to render one frame
-
-if [ ! -f "client.jar" ]; then
-    echo "SheepIt client not found. Downloading the client."
-    wget -O client.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
-    chmod +x client.jar
+#######################                                                                                                                 
+# System settings                                                                                                                       
+#######################                                                                                                                 
+mem="1" # Amount of RAM for SheepIt (in GB)                                                                                             
+gpu="--no-gpu" # if you have a GPU leave this empty                                                                                     
+computeMethod="CPU" # leave blank if you are unsure                                                                                     
+renderTime="" # The max time you are willing to commit to render one frame                                                              
+                                                                                                                                        
+if [ ! -f "client.jar" ]; then                                                                                                          
+    echo "SheepIt client not found. Downloading the client."                                                                            
+    wget -O client.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php                                                
+    chmod +x client.jar                                                                                                                 
+fi                                                                                                                                      
+                                                                                                                                        
+if [ ! -z $mem ]; then                                                                                                                  
+    mem="-memory ${mem}G"                                                                                                               
 fi
 
-# This will run the client with the settings above
-java -jar client.jar -memory $mem -ui text $gpu -cache-dir $HOME/sheepit-cache -compute-method $computeMethod -rendertime $renderTime -login $username -password $pass
+fi [ ! -z $computeMethod ]; then
+    computeMethod="-compute-method ${computeMethod}"
+fi
+                                                                                                                                        
+if [ ! -z $renderTime ]; then                                                                                                           
+    renderTime="-rendertime ${renderTime}"                                                                                              
+fi                                                                                                                             
+                                                                                                                                        
+# This will run the client with the settings above                                                                                      
+java -jar client.jar $mem -ui text $gpu -cache-dir $HOME/sheepit-cache $computeMethod $renderTime -login $username -password $pass
